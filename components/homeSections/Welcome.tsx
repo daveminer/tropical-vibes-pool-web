19e2b38dbee46f1564af01e8b8d929a5e7ecf677
Dave Miner <dave.miner@live.com>
2022-04-06 00:27:02 -0400

    add the landing page text

--- components/homeSections/Welcome.tsx
+++ components/homeSections/Welcome.tsx
@@ -5,8 +5,29 @@ import styles from "./Welcome.module.css";
 const Welcome = () => {
   return (
     <>
-      <div className={styles.landing}></div>
-      <div className={styles.landingCenter}></div>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600;800&display=swap" rel="stylesheet" />
+      <div className={{ position: 'relative' }}>
+        <div className={styles.navbar}></div>
+        <div className={styles.landing}></div>
+        <div className={styles.landingCenter}></div>
+        <div className={styles.titleBox}>
+          <div>
+            <div className={styles.titleTextHeader}>
+              Invest in a balanced
+            </div>
+            <div className={styles.titleText}>
+              Open and Sustainable Financial World with Cardano
+            </div>
+          </div>
+          <div className="v2_26">
+            <div className="v2_27">
+            </div>
+            <span className="v2_28"> Delegate Now</span>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
---
+++ next-env.d.ts
@@ -0,0 +1,2 @@
+// prevents css module imports from throwing an error against Typescript
+declare module '*.css';
